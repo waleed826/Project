32c0137a6e048034ff5e601626aab10bd06b16fd
waleed826 <waleedbinakbar@gmail.com>
2023-12-29 02:44:30 +0500

    one signals

--- App.js
+++ App.js
@@ -1,20 +1,93 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import Counter from './source/Redux/Counter'
-import { Provider } from 'react-redux'
-import { store } from './source/Redux/Store'
+// import { StyleSheet, Text, View } from 'react-native'
+// import React from 'react'
+// import Svg, { Path,Circle } from 'react-native-svg';
 
+// const App = () => {
+//   return (
+//     <View>
+//       <Svg width="100" height="100" viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg">
+//         {/* Left Part of G in Red */}
+//         <Path d='M20 10V70' stroke="rgba(234, 67, 53, 1)" strokeWidth="10" strokeLinejoin='round' strokeLinecap='butt'  />
+//         {/* <Circle cx="50" cy="50" r="40" fill="rgba(251, 188, 5, 1)" /> */}
+//         {/* Horizontal Part of G in Green */}
+//         <Path d="M20 50H40" stroke="rgba(52, 168, 83, 1)" strokeWidth="10" strokeLinecap='square' strokeLinejoin='round' st strokeLinecap='square' />
+
+//         {/* Right Part of G in Blue */}
+//         <Path d="M80 50V10" stroke="rgba(66, 133, 244, 1)" strokeWidth="10" strokeLinecap="round" />
+//       </Svg>
+//       <Svg height="100" width="100">
+//         <Path
+//           d="M20 10V90H80V50H50V30H80V10H20ZM50 30H30V50H50V30Z"
+//           fill="blue" strokeLinecap='square'
+//         />
+//       </Svg>
+//     </View>
+//   )
+// }
+
+// export default App
+
+// const styles = StyleSheet.create({})
+
+
+import { StyleSheet, Text, View } from 'react-native'
+import React, { useEffect } from 'react'
+import OneSignal from 'react-native-onesignal';
 const App = () => {
+
+  useEffect(()=>{
+    OneSignals()
+  },[])
+  const OneSignals = ()=>{
+// OneSignal Initialization
+OneSignal.setAppId('9446f177-17cd-4c86-8716-24250db9ec2d');
+
+// promptForPushNotificationsWithUserResponse will show the native iOS or Android notification permission prompt.
+// We recommend removing the following code and instead using an In-App Message to prompt for notification permission (See step 8)
+OneSignal.promptForPushNotificationsWithUserResponse();
+
+//Method for handling notifications received while app in foreground
+OneSignal.setNotificationWillShowInForegroundHandler(notificationReceivedEvent => {
+  console.log("OneSignal: notification will show in foreground:", notificationReceivedEvent);
+  let notification = notificationReceivedEvent.getNotification();
+  console.log("notification: ", notification.body);
+  // Complete with null means don't show a notification.
+  notificationReceivedEvent.complete(notification);
+});
+
+//Method for handling notifications opened
+OneSignal.setNotificationOpenedHandler(notification => {
+  console.log("OneSignal: notification opened:", notification);
+});
+  }
   return (
-    <Provider store={store}>
-    <Counter/>
-    </Provider>
+    <View>
+      <Text>App</Text>
+    </View>
   )
 }
 
 export default App
 
 const styles = StyleSheet.create({})
+
+// import { StyleSheet, Text, View } from 'react-native'
+// import React from 'react'
+// import Counter from './source/Redux/Counter'
+// import { Provider } from 'react-redux'
+// import { store } from './source/Redux/Store'
+
+// const App = () => {
+//   return (
+//     <Provider store={store}>
+//     <Counter/>
+//     </Provider>
+//   )
+// }
+
+// export default App
+
+// const styles = StyleSheet.create({})
 
 // import { StyleSheet, Text, View ,TextInput, Button,Image} from 'react-native'
 // import React, { useEffect, useState } from 'react'
